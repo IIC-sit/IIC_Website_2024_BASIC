46ccb9b840335e6abcb230189dc65715bf7f64a9
VEDANANDA N <120772722+VEDA117@users.noreply.github.com>
2024-12-30 13:03:37 +0530

    Add files via upload

---
+++ src/pages/Events.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Events = () => {
+  return (
+    <div className="events">
+      <h1>Upcoming Events</h1>
+
+      <div className="event-list">
+        <div className="event-box">
+          <h3>Recruitments</h3>
+          <p>January 02 and 03 2025</p>
+          <p>Join the Institution's Innovation Council to unlock your creativity, collaborate on groundbreaking ideas, and drive innovation that makes a difference!</p>
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSdPuBdN4s5mInDoZ1HwxE8qG_S2_iVyl1NvMV1BNyCdCAriWA/viewform?usp=sharing" target="_blank" rel="noopener noreferrer">See More</a>
+        </div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Events;
